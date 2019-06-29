10f887c8a779e8a2845cfb19f12e2c418a194d79
ukiyo <13175394+watanika@users.noreply.github.com>
2019-06-29 17:29:08 +0900

    [AC] abc130 abc130_a

---
+++ atcoder.jp/abc130/abc130_a/Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+    int X, A;
+    cin >> X >> A;
+    if (X < A)
+    {
+        cout << 0;
+    } else {
+        cout << 10;
+    }
+
+
+    return 0;
+}
