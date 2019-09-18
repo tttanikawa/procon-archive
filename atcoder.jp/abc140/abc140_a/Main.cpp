e90051afd64555154843ff5b6f23fae91b77b92a
ukiyo <13175394+watanika@users.noreply.github.com>
2019-09-18 14:18:04 +0900

    [AC] abc140 abc140_a

---
+++ atcoder.jp/abc140/abc140_a/Main.cpp
@@ -0,0 +1,48 @@
+#include <set>
+#include <map>
+#include <list>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <ctime>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <bitset>
+#include <cctype>
+#include <cstdlib>
+#include <cstring>
+#include <utility>
+#include <numeric>
+#include <complex>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <cassert>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+const double EPS = 1e-9;
+typedef vector<int> vint;
+typedef pair<int, int> pint;
+#define rep(i, n) REP(i, 0, n)
+#define ALL(v) v.begin(), v.end()
+#define MSG(a) cout << #a << " " << a << endl;
+#define REP(i, x, n) for(int i = x; i < n; i++)
+template<class T, class C> void chmax(T& a, C b){ a>b?:a=b; }
+template<class T, class C> void chmin(T& a, C b){ a<b?:a=b; }
+
+
+int main(int argc, char const *argv[])
+{
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	
+	int N;
+	cin >> N;
+
+	cout << N * N * N << endl;;
+	return 0;
+}
