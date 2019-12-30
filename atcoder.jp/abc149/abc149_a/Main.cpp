be55b870fc1e6a146bffe67c1363fbf1589c3757
ukiyo <13175394+watanika@users.noreply.github.com>
2019-12-30 13:54:38 +0900

    [AC] abc149 abc149_a

---
+++ atcoder.jp/abc149/abc149_a/Main.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <array>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <string>
+#include <stack>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+#define EPS (1e-7)
+#define INF (1e9)
+#define PI (acos(-1))
+
+#define MOD ((ll)1000000007)
+
+int main(int argc, char const *argv[])
+{
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	cout << fixed << setprecision(15);
+
+	string S, T;
+	cin >> S >> T;
+
+	cout << T << S << endl;
+	return 0;
+}
