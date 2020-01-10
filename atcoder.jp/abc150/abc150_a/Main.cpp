98bca72ef259a976a347ba596c1dc30711c6f122
ukiyo <13175394+watanika@users.noreply.github.com>
2020-01-10 21:07:10 +0900

    [AC] abc150 abc150_a

---
+++ atcoder.jp/abc150/abc150_a/Main.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+using pint = pair<int, int>;
+using pll = pair<ll, ll>;
+
+constexpr double EPS = 1e-7;
+constexpr int INF = 1e9;
+constexpr ll LINF = 1e18;
+#define PI (acos(-1))
+
+constexpr int MOD = 1e9 + 7;
+
+int main(int argc, char const *argv[]) {
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	cout << fixed << setprecision(15);
+
+	int K, X;
+	cin >> K >> X;
+
+	if (500 * K >= X) {
+		cout << "Yes" << endl;
+	} else {
+		cout << "No" << endl;
+	}
+
+	return 0;
+}
