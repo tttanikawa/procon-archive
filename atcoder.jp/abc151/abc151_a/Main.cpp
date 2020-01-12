e261481469ac3b792d826eaff4c0bb82b2be2101
ukiyo <13175394+watanika@users.noreply.github.com>
2020-01-12 21:03:19 +0900

    [AC] abc151 abc151_a

---
+++ atcoder.jp/abc151/abc151_a/Main.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+using pint = pair<int, int>;
+using pll = pair<ll, ll>;
+
+constexpr double EPS = 1e-7;
+constexpr int INF = 1e9;
+constexpr ll LINF = 1e18;
+#define PI (acos(-1))
+
+constexpr int MOD = 1e9 + 7;
+
+int main(int argc, char const *argv[]) {
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	cout << fixed << setprecision(15);
+
+	char C;
+	cin >> C;
+	cout << (char)(C + 1) << endl;
+	return 0;
+}
