c485358bd342cbb56dbf8f02c6f323141061b08d
ukiyo <13175394+watanika@users.noreply.github.com>
2020-01-26 21:02:02 +0900

    [AC] abc153 abc153_a

---
+++ atcoder.jp/abc153/abc153_a/Main.cpp
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+using pint = pair<int, int>;
+using pll = pair<ll, ll>;
+
+constexpr double EPS = 1e-7;
+constexpr int INF = 1e9;
+#define PI (acos(-1))
+
+constexpr int MOD = 1e9 + 7;
+
+int main(int argc, char const *argv[]) {
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	cout << fixed << setprecision(15);
+
+	int H, A;
+	cin >> H >> A;
+	if (H % A == 0) {
+		cout << H / A << endl;
+	} else {
+		cout << H / A + 1 << endl;
+	}
+
+	return 0;
+}
