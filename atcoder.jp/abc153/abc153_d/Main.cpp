abcbcf02aa4e188840c8c7feb2f2e77dfe4f7f53
ukiyo <13175394+watanika@users.noreply.github.com>
2020-01-26 21:16:16 +0900

    [AC] abc153 abc153_d

---
+++ atcoder.jp/abc153/abc153_d/Main.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <array>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+using pint = pair<int, int>;
+using pll = pair<ll, ll>;
+
+constexpr double EPS = 1e-7;
+constexpr int INF = 1e9;
+#define PI (acos(-1))
+
+constexpr int MOD = 1e9 + 7;
+
+int main(int argc, char const *argv[]) {
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	cout << fixed << setprecision(15);
+
+	ll H;
+	cin >> H;
+
+	ll d = floor(log2(H));
+	ll ans = 0;
+	for (ll i = 0; i < d + 1; i++) {
+		ans += pow(2, i);
+	}
+	cout << ans << endl;
+	return 0;
+}
