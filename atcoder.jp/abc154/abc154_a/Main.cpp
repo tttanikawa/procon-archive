ac50d2a83033f4802d150febcd505005a974d695
ukiyo <13175394+watanika@users.noreply.github.com>
2020-02-09 21:02:49 +0900

    [AC] abc154 abc154_a

---
+++ atcoder.jp/abc154/abc154_a/Main.cpp
@@ -0,0 +1,84 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+using pint = pair<int, int>;
+using pll = pair<ll, ll>;
+
+constexpr double EPS = 1e-7;
+
+template <typename T>
+constexpr T INF = numeric_limits<T>::max();
+
+template <class T>
+constexpr T PI = static_cast<T>(3.14159265358979323846);
+
+constexpr int MOD = 1e9 + 7;
+
+template <class T>
+bool chmin(T &a, T b) {
+	if (a > b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+
+template <class T>
+bool chmax(T &a, T b) {
+	if (a < b) {
+		a = b;
+		return true;
+	}
+	return false;
+}
+
+template <typename T>
+std::vector<T> table(int n, T v) { return std::vector<T>(n, v); }
+
+template <class... Args>
+auto table(int n, Args... args) {
+	auto val = table(args...);
+	return std::vector<decltype(val)>(n, std::move(val));
+}
+
+int main(int argc, char const *argv[]) {
+	cin.tie(nullptr);
+	ios::sync_with_stdio(false);
+	cout << fixed << setprecision(15);
+
+	string S, T, U;
+	int A, B;
+	cin >> S >> T >> A >> B >> U;
+	if (S == U) {
+		cout << A - 1 << " " << B << endl;
+	} else if (T == U) {
+		cout << A << " " << B - 1 << endl;
+	} else {
+		cout << A << " " << B << endl;
+	}
+
+	return 0;
+}
