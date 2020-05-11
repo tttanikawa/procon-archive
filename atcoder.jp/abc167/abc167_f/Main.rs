60426fe9781a84d65a69d399fffc77a7678e7a01
ukiyo <13175394+watanika@users.noreply.github.com>
2020-05-11 22:07:08 +0900

    [AC] abc167 abc167_f

---
+++ atcoder.jp/abc167/abc167_f/Main.rs
@@ -0,0 +1,225 @@
+#[allow(unused_imports)]
+use std::cmp;
+
+fn check(s: &Vec<char>) -> (i64, i64) {
+    let mut min_v = 0;
+    let mut total = 0;
+    for &c in s.iter() {
+        if c == '(' {
+            total += 1;
+        } else {
+            total -= 1;
+            min_v = cmp::min(min_v, total);
+        }
+    }
+    (min_v, total)
+}
+
+#[test]
+fn check_test() {
+    assert_eq!(check(&"()".chars().collect::<Vec<char>>()), (0, 0));
+    assert_eq!(check(&")(".chars().collect::<Vec<char>>()), (1, 1));
+    assert_eq!(check(&")((())".chars().collect::<Vec<char>>()), (1, 1));
+    assert_eq!(check(&"()))(((".chars().collect::<Vec<char>>()), (2, 3));
+}
+
+fn run() {
+    let (r, w) = (std::io::stdin(), std::io::stdout());
+    let mut sc = IO::new(r.lock(), w.lock());
+
+    let n: usize = sc.read();
+
+    let mut pos = vec![];
+    let mut neg = vec![];
+    for _ in 0..n {
+        let s = sc.read_chars();
+        let (min_v, total) = check(&s);
+        if total >= 0 {
+            pos.push((-min_v, total));
+        } else {
+            neg.push((-(min_v - total), -total));
+        }
+    }
+
+    pos.sort_by_key(|&(min_v, total)| min_v);
+    neg.sort_by_key(|&(min_v, total)| min_v);
+    // neg.reverse();
+
+    let mut left_stacked = 0;
+    for (min_v, total) in pos.into_iter() {
+        if min_v > left_stacked {
+            println!("No");
+            return;
+        }
+        left_stacked += total;
+    }
+
+    let mut right_stacked = 0;
+    for (min_v, total) in neg.into_iter() {
+        if min_v > right_stacked {
+            println!("No");
+            return;
+        }
+        right_stacked += total.abs();
+    }
+
+    let ans = left_stacked == right_stacked;
+    yn(ans);
+}
+
+fn main() {
+    std::thread::Builder::new()
+        .name("run".into())
+        .stack_size(256 * 1024 * 1024)
+        .spawn(run)
+        .unwrap()
+        .join()
+        .unwrap();
+}
+
+pub struct IO<R, W: std::io::Write>(R, std::io::BufWriter<W>);
+
+impl<R: std::io::Read, W: std::io::Write> IO<R, W> {
+    pub fn new(r: R, w: W) -> IO<R, W> {
+        IO(r, std::io::BufWriter::new(w))
+    }
+    pub fn write<S: std::ops::Deref<Target = str>>(&mut self, s: S) {
+        use std::io::Write;
+        self.1.write(s.as_bytes()).unwrap();
+    }
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .0
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r' || b == b'\t')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r' && b != b'\t')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn read_vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn read_pairs<T: std::str::FromStr>(&mut self, n: usize) -> Vec<(T, T)> {
+        (0..n).map(|_| (self.read(), self.read())).collect()
+    }
+    pub fn read_chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
+    pub fn read_char_grid(&mut self, n: usize) -> Vec<Vec<char>> {
+        (0..n).map(|_| self.read_chars()).collect()
+    }
+    pub fn read_matrix<T: std::str::FromStr>(&mut self, n: usize, m: usize) -> Vec<Vec<T>> {
+        (0..n)
+            .map(|_| (0..m).map(|_| self.read()).collect())
+            .collect()
+    }
+}
+
+#[allow(dead_code)]
+fn yn(flag: bool) {
+    if flag {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
+
+use std::ops::{Add, Div};
+
+/// Generic binary search
+///
+/// # Arguments
+///
+/// * `l` - the value assumes to `pred(l) = true`
+/// * `r` - the value assumes to `pred(r) = false`
+/// * `pred` - predicate for binary search
+///
+/// # Returns
+///
+/// * `ret` where `pred(ret) = true` && `pred(ret + delta) = false`
+///
+/// # Note
+///
+/// `pred(l)` and `pred(r)` are not called. `pred` is called only values in the range `(l, r)`.
+///
+pub fn binary_search<T: Add<Output = T> + Div<Output = T> + PartialEq + From<u8> + Copy>(
+    l: T,
+    r: T,
+    pred: impl Fn(T) -> bool,
+) -> T {
+    let mut l = l;
+    let mut r = r;
+    let two = T::from(2_u8);
+    loop {
+        let m = (l + r) / two;
+        if l == m || r == m {
+            break l;
+        }
+        if pred(m) {
+            l = m;
+        } else {
+            r = m;
+        }
+    }
+}
+
+#[test]
+fn binary_search_test() {
+    let v = [1, 2, 3, 4, 5];
+    assert_eq!(3, binary_search(v.len() as _, -1, |i| v[i as usize] > 3));
+    assert_eq!(5, binary_search(v.len() as _, -1, |i| v[i as usize] > 100));
+    assert_eq!(0, binary_search(v.len() as _, -1, |i| v[i as usize] > 0));
+}
+
+/// Returns index to the first element in `v` which does not compare less than `val`.
+pub fn lower_bound<T: PartialOrd>(v: &[T], val: &T) -> usize {
+    (binary_search(-1, v.len() as i64, |i: i64| v[i as usize] < *val) + 1) as usize
+}
+
+#[test]
+fn lower_bound_test() {
+    let v = vec![1, 3, 3, 4, 5];
+    assert_eq!(lower_bound(&v, &0), 0);
+    assert_eq!(lower_bound(&v, &1), 0);
+    assert_eq!(lower_bound(&v, &2), 1);
+    assert_eq!(lower_bound(&v, &3), 1);
+    assert_eq!(lower_bound(&v, &4), 3);
+    assert_eq!(lower_bound(&v, &5), 4);
+    assert_eq!(lower_bound(&v, &999), 5);
+}
+
+/// Returns index to the first element in `v` which compares greater than `val`.
+pub fn upper_bound<T: PartialOrd>(v: &[T], val: &T) -> usize {
+    (binary_search(-1, v.len() as i64, |i: i64| v[i as usize] <= *val) + 1) as usize
+}
+
+#[test]
+fn upper_bound_test() {
+    let v: &[i32] = &[1, 3, 3, 4, 5];
+    assert_eq!(upper_bound(v, &0), 0);
+    assert_eq!(upper_bound(v, &1), 1);
+    assert_eq!(upper_bound(v, &2), 1);
+    assert_eq!(upper_bound(v, &3), 3);
+    assert_eq!(upper_bound(v, &4), 4);
+    assert_eq!(upper_bound(v, &5), 5);
+    assert_eq!(upper_bound(v, &999), 5);
+}
+
+/// Returns `(lower_bound(v, val), upper_bound(v, val))`
+pub fn equal_range<T: PartialOrd>(v: &[T], val: &T) -> (usize, usize) {
+    (lower_bound(v, val), upper_bound(v, val))
+}
+
+pub fn gcd(a: usize, b: usize) -> usize {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
