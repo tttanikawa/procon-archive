5544206ba1bea66a28bae056e8a0ca2a34bee22a
ukiyo <13175394+watanika@users.noreply.github.com>
2020-08-08 00:22:50 +0900

    [AC] agc043 agc043_a

---
+++ atcoder.jp/agc043/agc043_a/Main.rs
@@ -0,0 +1,105 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use itertools_num::ItertoolsNum;
+#[allow(unused_imports)]
+use std::cmp;
+#[allow(unused_imports)]
+use std::iter;
+#[allow(unused_imports)]
+use superslice::*;
+
+fn run() {
+    let (r, w) = (std::io::stdin(), std::io::stdout());
+    let mut sc = IO::new(r.lock(), w.lock());
+
+    let h: usize = sc.read();
+    let w: usize = sc.read();
+    let s = sc.read_char_grid(h);
+
+    let mut dp = vec![vec![std::usize::MAX; w]; h];
+    if s[0][0] == '#' {
+        dp[0][0] = 1;
+    } else {
+        dp[0][0] = 0;
+    }
+
+    for i in 0..h {
+        for j in 0..w {
+            if i + 1 < h {
+                dp[i + 1][j] = if s[i + 1][j] == '#' && s[i][j] == '.' {
+                    cmp::min(dp[i + 1][j], dp[i][j] + 1)
+                } else {
+                    cmp::min(dp[i + 1][j], dp[i][j])
+                };
+            }
+            if j + 1 < w {
+                dp[i][j + 1] = if s[i][j + 1] == '#' && s[i][j] == '.' {
+                    cmp::min(dp[i][j + 1], dp[i][j] + 1)
+                } else {
+                    cmp::min(dp[i][j + 1], dp[i][j])
+                };
+            }
+        }
+    }
+    println!("{}", dp[h - 1][w - 1]);
+}
+
+fn main() {
+    std::thread::Builder::new()
+        .name("run".into())
+        .stack_size(256 * 1024 * 1024)
+        .spawn(run)
+        .unwrap()
+        .join()
+        .unwrap();
+}
+
+pub struct IO<R, W: std::io::Write>(R, std::io::BufWriter<W>);
+
+impl<R: std::io::Read, W: std::io::Write> IO<R, W> {
+    pub fn new(r: R, w: W) -> IO<R, W> {
+        IO(r, std::io::BufWriter::new(w))
+    }
+    pub fn write<S: std::ops::Deref<Target = str>>(&mut self, s: S) {
+        use std::io::Write;
+        self.1.write(s.as_bytes()).unwrap();
+    }
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .0
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r' || b == b'\t')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r' && b != b'\t')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn read_vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn read_pairs<T: std::str::FromStr>(&mut self, n: usize) -> Vec<(T, T)> {
+        (0..n).map(|_| (self.read(), self.read())).collect()
+    }
+    pub fn read_pairs_1_indexed(&mut self, n: usize) -> Vec<(usize, usize)> {
+        (0..n)
+            .map(|_| (self.read::<usize>() - 1, self.read::<usize>() - 1))
+            .collect()
+    }
+    pub fn read_chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
+    pub fn read_char_grid(&mut self, n: usize) -> Vec<Vec<char>> {
+        (0..n).map(|_| self.read_chars()).collect()
+    }
+    pub fn read_matrix<T: std::str::FromStr>(&mut self, n: usize, m: usize) -> Vec<Vec<T>> {
+        (0..n)
+            .map(|_| (0..m).map(|_| self.read()).collect())
+            .collect()
+    }
+}
