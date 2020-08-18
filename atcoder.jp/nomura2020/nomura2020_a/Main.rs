b199b004065a398ce56ff7ef0b1a969d311f5c4a
ukiyo <13175394+watanika@users.noreply.github.com>
2020-08-19 01:31:39 +0900

    [AC] nomura2020 nomura2020_a

---
+++ atcoder.jp/nomura2020/nomura2020_a/Main.rs
@@ -0,0 +1,89 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use itertools_num::ItertoolsNum;
+#[allow(unused_imports)]
+use std::cmp;
+#[allow(unused_imports)]
+use std::iter;
+#[allow(unused_imports)]
+use superslice::*;
+
+fn run() {
+    let (r, w) = (std::io::stdin(), std::io::stdout());
+    let mut sc = IO::new(r.lock(), w.lock());
+
+    let h1: usize = sc.read();
+    let m1: usize = sc.read();
+    let h2: usize = sc.read();
+    let m2: usize = sc.read();
+    let k: usize = sc.read();
+
+    let minutes1 = h1 * 60 + m1;
+    let minutes2 = h2 * 60 + m2;
+    let ans = if minutes1 < minutes2 {
+        minutes2 - minutes1
+    } else {
+        60 * 24 - minutes1 + minutes2
+    } - k;
+    println!("{}", ans);
+}
+
+fn main() {
+    std::thread::Builder::new()
+        .name("run".into())
+        .stack_size(256 * 1024 * 1024)
+        .spawn(run)
+        .unwrap()
+        .join()
+        .unwrap();
+}
+
+pub struct IO<R, W: std::io::Write>(R, std::io::BufWriter<W>);
+
+impl<R: std::io::Read, W: std::io::Write> IO<R, W> {
+    pub fn new(r: R, w: W) -> IO<R, W> {
+        IO(r, std::io::BufWriter::new(w))
+    }
+    pub fn write<S: std::ops::Deref<Target = str>>(&mut self, s: S) {
+        use std::io::Write;
+        self.1.write(s.as_bytes()).unwrap();
+    }
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .0
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r' || b == b'\t')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r' && b != b'\t')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn read_vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn read_pairs<T: std::str::FromStr>(&mut self, n: usize) -> Vec<(T, T)> {
+        (0..n).map(|_| (self.read(), self.read())).collect()
+    }
+    pub fn read_pairs_1_indexed(&mut self, n: usize) -> Vec<(usize, usize)> {
+        (0..n)
+            .map(|_| (self.read::<usize>() - 1, self.read::<usize>() - 1))
+            .collect()
+    }
+    pub fn read_chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
+    pub fn read_char_grid(&mut self, n: usize) -> Vec<Vec<char>> {
+        (0..n).map(|_| self.read_chars()).collect()
+    }
+    pub fn read_matrix<T: std::str::FromStr>(&mut self, n: usize, m: usize) -> Vec<Vec<T>> {
+        (0..n)
+            .map(|_| (0..m).map(|_| self.read()).collect())
+            .collect()
+    }
+}
