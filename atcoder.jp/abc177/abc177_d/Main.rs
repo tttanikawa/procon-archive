521c8fa6c1eafbff352229f8a89d3648ad2e8e44
ukiyo <13175394+watanika@users.noreply.github.com>
2020-08-29 21:20:45 +0900

    [AC] abc177 abc177_d

---
+++ atcoder.jp/abc177/abc177_d/Main.rs
@@ -0,0 +1,132 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use itertools_num::ItertoolsNum;
+#[allow(unused_imports)]
+use std::cmp;
+#[allow(unused_imports)]
+use std::iter;
+#[allow(unused_imports)]
+use superslice::*;
+
+fn run() {
+    let (r, w) = (std::io::stdin(), std::io::stdout());
+    let mut sc = IO::new(r.lock(), w.lock());
+
+    let n: usize = sc.read();
+    let m: usize = sc.read();
+    let ab = sc.read_pairs_1_indexed(m);
+
+    let mut uf = UnionFind::new(n);
+    for (a, b) in ab.into_iter() {
+        uf.unite(a, b);
+    }
+
+    let ans = uf.sizes.into_iter().max().unwrap();
+    println!("{}", ans);
+}
+
+fn main() {
+    std::thread::Builder::new()
+        .name("run".into())
+        .stack_size(256 * 1024 * 1024)
+        .spawn(run)
+        .unwrap()
+        .join()
+        .unwrap();
+}
+
+pub struct UnionFind {
+    parent: Vec<usize>,
+    sizes: Vec<usize>,
+    size: usize,
+}
+
+impl UnionFind {
+    pub fn new(n: usize) -> UnionFind {
+        UnionFind {
+            parent: (0..n).map(|i| i).collect::<Vec<usize>>(),
+            sizes: vec![1; n],
+            size: n,
+        }
+    }
+
+    pub fn find(&mut self, x: usize) -> usize {
+        if x == self.parent[x] {
+            x
+        } else {
+            let px = self.parent[x];
+            self.parent[x] = self.find(px);
+            self.parent[x]
+        }
+    }
+
+    pub fn unite(&mut self, x: usize, y: usize) -> bool {
+        let parent_x = self.find(x);
+        let parent_y = self.find(y);
+        if parent_x == parent_y {
+            return false;
+        }
+
+        let (large, small) = if self.sizes[parent_x] < self.sizes[parent_y] {
+            (parent_y, parent_x)
+        } else {
+            (parent_x, parent_y)
+        };
+
+        self.parent[small] = large;
+        self.sizes[large] += self.sizes[small];
+        self.sizes[small] = 0;
+        self.size -= 1;
+        true
+    }
+}
+
+pub struct IO<R, W: std::io::Write>(R, std::io::BufWriter<W>);
+
+impl<R: std::io::Read, W: std::io::Write> IO<R, W> {
+    pub fn new(r: R, w: W) -> IO<R, W> {
+        IO(r, std::io::BufWriter::new(w))
+    }
+    pub fn write<S: std::ops::Deref<Target = str>>(&mut self, s: S) {
+        use std::io::Write;
+        self.1.write(s.as_bytes()).unwrap();
+    }
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .0
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r' || b == b'\t')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r' && b != b'\t')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn read_vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn read_pairs<T: std::str::FromStr>(&mut self, n: usize) -> Vec<(T, T)> {
+        (0..n).map(|_| (self.read(), self.read())).collect()
+    }
+    pub fn read_pairs_1_indexed(&mut self, n: usize) -> Vec<(usize, usize)> {
+        (0..n)
+            .map(|_| (self.read::<usize>() - 1, self.read::<usize>() - 1))
+            .collect()
+    }
+    pub fn read_chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
+    pub fn read_char_grid(&mut self, n: usize) -> Vec<Vec<char>> {
+        (0..n).map(|_| self.read_chars()).collect()
+    }
+    pub fn read_matrix<T: std::str::FromStr>(&mut self, n: usize, m: usize) -> Vec<Vec<T>> {
+        (0..n)
+            .map(|_| (0..m).map(|_| self.read()).collect())
+            .collect()
+    }
+}
