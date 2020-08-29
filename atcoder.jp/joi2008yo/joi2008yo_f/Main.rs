f4f8da67cbbacd2c934170eecd39d152be5826ef
ukiyo <13175394+watanika@users.noreply.github.com>
2020-08-29 17:46:58 +0900

    [AC] joi2008yo joi2008yo_f

---
+++ atcoder.jp/joi2008yo/joi2008yo_f/Main.rs
@@ -0,0 +1,123 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use itertools_num::ItertoolsNum;
+#[allow(unused_imports)]
+use std::cmp;
+use std::collections::BinaryHeap;
+#[allow(unused_imports)]
+use std::iter;
+#[allow(unused_imports)]
+use superslice::*;
+
+fn run() {
+    let (r, w) = (std::io::stdin(), std::io::stdout());
+    let mut sc = IO::new(r.lock(), w.lock());
+
+    let n: usize = sc.read();
+    let k: usize = sc.read();
+
+    let mut g = vec![vec![]; n];
+
+    for _ in 0..k {
+        let x = sc.read::<usize>();
+        if x == 0 {
+            let mut heap = BinaryHeap::new();
+            let mut dist = vec![vec![std::i64::MAX; n]; n];
+            for i in 0..n {
+                dist[i][i] = 0;
+            }
+            let a = sc.read::<usize>() - 1;
+            let b = sc.read::<usize>() - 1;
+            heap.push((0_i64, a));
+            while let Some((total, cur)) = heap.pop() {
+                let total = -total;
+                if cur == b {
+                    break;
+                }
+                if total > dist[a][cur] {
+                    continue;
+                }
+
+                for &(next, d) in g[cur].iter() {
+                    if total + d < dist[a][next] {
+                        heap.push((-total - d, next));
+                        dist[a][next] = total + d;
+                        dist[next][a] = total + d;
+                    }
+                }
+            }
+
+            if dist[a][b] == std::i64::MAX {
+                println!("-1");
+            } else {
+                println!("{}", dist[a][b]);
+            }
+        } else {
+            let c = sc.read::<usize>() - 1;
+            let d = sc.read::<usize>() - 1;
+            let e = sc.read::<i64>();
+            g[c].push((d, e));
+            g[d].push((c, e));
+        }
+    }
+}
+
+fn main() {
+    std::thread::Builder::new()
+        .name("run".into())
+        .stack_size(256 * 1024 * 1024)
+        .spawn(run)
+        .unwrap()
+        .join()
+        .unwrap();
+}
+
+pub struct IO<R, W: std::io::Write>(R, std::io::BufWriter<W>);
+
+impl<R: std::io::Read, W: std::io::Write> IO<R, W> {
+    pub fn new(r: R, w: W) -> IO<R, W> {
+        IO(r, std::io::BufWriter::new(w))
+    }
+    pub fn write<S: std::ops::Deref<Target = str>>(&mut self, s: S) {
+        use std::io::Write;
+        self.1.write(s.as_bytes()).unwrap();
+    }
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .0
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r' || b == b'\t')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r' && b != b'\t')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn read_vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn read_pairs<T: std::str::FromStr>(&mut self, n: usize) -> Vec<(T, T)> {
+        (0..n).map(|_| (self.read(), self.read())).collect()
+    }
+    pub fn read_pairs_1_indexed(&mut self, n: usize) -> Vec<(usize, usize)> {
+        (0..n)
+            .map(|_| (self.read::<usize>() - 1, self.read::<usize>() - 1))
+            .collect()
+    }
+    pub fn read_chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
+    pub fn read_char_grid(&mut self, n: usize) -> Vec<Vec<char>> {
+        (0..n).map(|_| self.read_chars()).collect()
+    }
+    pub fn read_matrix<T: std::str::FromStr>(&mut self, n: usize, m: usize) -> Vec<Vec<T>> {
+        (0..n)
+            .map(|_| (0..m).map(|_| self.read()).collect())
+            .collect()
+    }
+}
