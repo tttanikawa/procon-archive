34a18349608b603651e300cbf1fc68724ab359f9
ukiyo <13175394+watanika@users.noreply.github.com>
2020-09-01 02:17:48 +0900

    [AC] code-festival-2016-quala codefestival_2016_qualA_c

---
+++ atcoder.jp/code-festival-2016-quala/codefestival_2016_qualA_c/Main.rs
@@ -0,0 +1,100 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use itertools_num::ItertoolsNum;
+#[allow(unused_imports)]
+use std::cmp;
+#[allow(unused_imports)]
+use std::iter;
+#[allow(unused_imports)]
+use superslice::*;
+
+fn run() {
+    let (r, w) = (std::io::stdin(), std::io::stdout());
+    let mut sc = IO::new(r.lock(), w.lock());
+
+    let s = sc.read_chars();
+    let mut k: usize = sc.read();
+
+    let n = s.len();
+    let mut ans = vec![];
+    for i in 0..n {
+        let d = s[i] as usize - 'a' as usize;
+        let update = 26 - d;
+        if d == 0 {
+            ans.push(s[i]);
+        } else if 26 <= k + d {
+            ans.push('a');
+            k -= update;
+        } else {
+            ans.push(s[i]);
+        }
+    }
+    if k > 0 {
+        // let n = ans.len();
+        // let s_n = ans[n - 1];
+        // let d = s_n as usize - 'a' as usize;
+        // ans[n - 1] = (((d + k) % 26) as u8 + 'a' as u8) as char;
+        ans[n - 1] = ('a' as u8 + ((ans[n - 1] as u8 - 'a' as u8) + (k % 26) as u8) % 26) as char;
+    }
+    println!("{}", ans.iter().collect::<String>());
+}
+
+fn main() {
+    std::thread::Builder::new()
+        .name("run".into())
+        .stack_size(256 * 1024 * 1024)
+        .spawn(run)
+        .unwrap()
+        .join()
+        .unwrap();
+}
+
+pub struct IO<R, W: std::io::Write>(R, std::io::BufWriter<W>);
+
+impl<R: std::io::Read, W: std::io::Write> IO<R, W> {
+    pub fn new(r: R, w: W) -> IO<R, W> {
+        IO(r, std::io::BufWriter::new(w))
+    }
+    pub fn write<S: std::ops::Deref<Target = str>>(&mut self, s: S) {
+        use std::io::Write;
+        self.1.write(s.as_bytes()).unwrap();
+    }
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .0
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r' || b == b'\t')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r' && b != b'\t')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn read_vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn read_pairs<T: std::str::FromStr>(&mut self, n: usize) -> Vec<(T, T)> {
+        (0..n).map(|_| (self.read(), self.read())).collect()
+    }
+    pub fn read_pairs_1_indexed(&mut self, n: usize) -> Vec<(usize, usize)> {
+        (0..n)
+            .map(|_| (self.read::<usize>() - 1, self.read::<usize>() - 1))
+            .collect()
+    }
+    pub fn read_chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
+    pub fn read_char_grid(&mut self, n: usize) -> Vec<Vec<char>> {
+        (0..n).map(|_| self.read_chars()).collect()
+    }
+    pub fn read_matrix<T: std::str::FromStr>(&mut self, n: usize, m: usize) -> Vec<Vec<T>> {
+        (0..n)
+            .map(|_| (0..m).map(|_| self.read()).collect())
+            .collect()
+    }
+}
