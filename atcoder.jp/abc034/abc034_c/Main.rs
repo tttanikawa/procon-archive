549bb2276cb4d6b424051e1acdbbab9ae0556c0c
ukiyo <13175394+watanika@users.noreply.github.com>
2020-09-02 01:00:10 +0900

    [AC] abc034 abc034_c

---
+++ atcoder.jp/abc034/abc034_c/Main.rs
@@ -0,0 +1,121 @@
+#[allow(unused_imports)]
+use itertools::Itertools;
+#[allow(unused_imports)]
+use itertools_num::ItertoolsNum;
+#[allow(unused_imports)]
+use std::cmp;
+#[allow(unused_imports)]
+use std::iter;
+#[allow(unused_imports)]
+use superslice::*;
+
+pub struct Combination {
+    fact: Vec<usize>,
+    inv_fact: Vec<usize>,
+    modulo: usize,
+}
+
+impl Combination {
+    pub fn new(max: usize, modulo: usize) -> Self {
+        let mut inv = vec![0; max + 1];
+        let mut fact = vec![0; max + 1];
+        let mut inv_fact = vec![0; max + 1];
+        inv[1] = 1;
+        for i in 2..(max + 1) {
+            inv[i] = inv[modulo % i] * (modulo - modulo / i) % modulo;
+        }
+        fact[0] = 1;
+        inv_fact[0] = 1;
+        for i in 0..max {
+            fact[i + 1] = fact[i] * (i + 1) % modulo;
+        }
+        for i in 0..max {
+            inv_fact[i + 1] = inv_fact[i] * inv[i + 1] % modulo;
+        }
+        Self {
+            fact,
+            inv_fact,
+            modulo,
+        }
+    }
+
+    pub fn get(&self, x: usize, y: usize) -> usize {
+        assert!(x >= y);
+        self.fact[x] * self.inv_fact[y] % self.modulo * self.inv_fact[x - y] % self.modulo
+    }
+
+    pub fn h(&self, n: usize, r: usize) -> usize {
+        self.get(n + r - 1, r)
+    }
+}
+
+fn run() {
+    let (r, w) = (std::io::stdin(), std::io::stdout());
+    let mut sc = IO::new(r.lock(), w.lock());
+
+    let w: usize = sc.read();
+    let h: usize = sc.read();
+
+    let comb = Combination::new(w + h - 2, 1e9 as usize + 7);
+    let ans = comb.get(w + h - 2, h - 1);
+    println!("{}", ans);
+}
+
+fn main() {
+    std::thread::Builder::new()
+        .name("run".into())
+        .stack_size(256 * 1024 * 1024)
+        .spawn(run)
+        .unwrap()
+        .join()
+        .unwrap();
+}
+
+pub struct IO<R, W: std::io::Write>(R, std::io::BufWriter<W>);
+
+impl<R: std::io::Read, W: std::io::Write> IO<R, W> {
+    pub fn new(r: R, w: W) -> IO<R, W> {
+        IO(r, std::io::BufWriter::new(w))
+    }
+    pub fn write<S: std::ops::Deref<Target = str>>(&mut self, s: S) {
+        use std::io::Write;
+        self.1.write(s.as_bytes()).unwrap();
+    }
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .0
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r' || b == b'\t')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r' && b != b'\t')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn read_vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn read_pairs<T: std::str::FromStr>(&mut self, n: usize) -> Vec<(T, T)> {
+        (0..n).map(|_| (self.read(), self.read())).collect()
+    }
+    pub fn read_pairs_1_indexed(&mut self, n: usize) -> Vec<(usize, usize)> {
+        (0..n)
+            .map(|_| (self.read::<usize>() - 1, self.read::<usize>() - 1))
+            .collect()
+    }
+    pub fn read_chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
+    pub fn read_char_grid(&mut self, n: usize) -> Vec<Vec<char>> {
+        (0..n).map(|_| self.read_chars()).collect()
+    }
+    pub fn read_matrix<T: std::str::FromStr>(&mut self, n: usize, m: usize) -> Vec<Vec<T>> {
+        (0..n)
+            .map(|_| (0..m).map(|_| self.read()).collect())
+            .collect()
+    }
+}
